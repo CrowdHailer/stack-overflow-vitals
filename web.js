72041ee06aee2a5e985ae5c6ac1a0bf5b911e89c
Peter <peterhsaxton@gmail.com>
2014-03-27 07:51:11 +0000

    Request stackoverflow data ISSUE data returned gziped

--- web.js
+++ web.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var logfmt = require("logfmt");
+var request = require('request');
+
 var app = express();
 
 app.use(logfmt.requestLogger());
@@ -10,7 +12,16 @@ app.get('/', function(req, res) {
 
 app.get('/:id', function (req, res){
   var id = req.params.id;
-  console.log(id);
+  request('https://api.stackexchange.com/2.1/users/1187299/?site=stackoverflow', function (error, response, body) {
+    console.log('first')
+    if (!error && response.statusCode == 200) {
+      console.log('hello')
+      console.log(response.toJSON());
+      console.log(body.toJSON);
+    }
+  });
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ a: 1 }));
 });
 
 var port = Number(process.env.PORT || 5000);
