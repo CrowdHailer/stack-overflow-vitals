608ec8f0ed24047f9dd32170b216cc1840ba7177
Peter <peterhsaxton@gmail.com>
2014-03-27 08:25:42 +0000

    Experiments and comments to unzip response

--- web.js
+++ web.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var request = require('request');
+var zlib = require('zlib');
+// var gzip = zlib.createGzip();
 
 var app = express();
 
@@ -15,13 +17,23 @@ app.get('/:id', function (req, res){
   request('https://api.stackexchange.com/2.1/users/1187299/?site=stackoverflow', function (error, response, body) {
     console.log('first')
     if (!error && response.statusCode == 200) {
-      console.log('hello')
-      console.log(response.toJSON());
-      console.log(body.toJSON);
+      // console.log('hello');
+      console.log(response.headers['content-encoding']);
+      var buffer = new Buffer('eJzT0yMAAGTvBe8=', 'utf-8');
+      // console.log(buffer);
+      // console.log(response.headers);
+      zlib.gunzip(buffer, function(err, decoded) {
+        console.log(err);
+        console.log(decoded);
+        console.log(decoded.toString);
+
+        // callback(err, decoded && decoded.toString());
+      });
+      // console.log(zlib.unzip(response));
     }
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ a: 1 }));
   });
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({ a: 1 }));
 });
 
 var port = Number(process.env.PORT || 5000);
